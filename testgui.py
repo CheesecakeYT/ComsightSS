b436c126e79fb647e7b9e4bfbda45d17d0e1ae5d
MarusidloRex <89594877+MarusidloRex@users.noreply.github.com>
2023-08-11 09:51:31 +0200

    Update testgui.py

--- testgui.py
+++ testgui.py
@@ -19,3 +19,32 @@ button.pack()
 
 # Start the GUI event loop
 root.mainloop()
+
+
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+def scan_file():
+    file_path = filedialog.askopenfilename(title="Select a file to scan")
+    if file_path:
+        result_label.config(text=f"Scanning {os.path.basename(file_path)}...")
+        # Implement your scanning logic here
+        # Display scan results in the result_label
+
+# Create the main window
+root = tk.Tk()
+root.title("Comsight SecurityShield")
+
+# Create widgets
+title_label = tk.Label(root, text="Welcome to Simple Antivirus")
+scan_button = tk.Button(root, text="Scan File", command=scan_file)
+result_label = tk.Label(root, text="")
+
+# Pack widgets
+title_label.pack(pady=10)
+scan_button.pack(pady=10)
+result_label.pack()
+
+# Start GUI event loop
+root.mainloop()
